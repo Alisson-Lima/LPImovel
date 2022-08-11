6b40adb4d12b4677e2bfb018a26f50e9a42d1321
Alisson Renan <renalima.rl46@gmail.com>
2022-08-11 18:09:21 -0300

    Fixing bugs

--- src/components/Place/index.jsx
+++ src/components/Place/index.jsx
@@ -1,5 +1,4 @@
 import "./style.css"
-import "./fotoParis.png"
 export function Place(){
     return <div className="place-image"><img src="./src/assets/images/Paris.png" alt="Imagem do lugar"/></div>
 }
