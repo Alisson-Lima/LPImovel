4fc2985b65e98cc43059e0e41e3f849c8776655f
Alisson Renan <renalima.rl46@gmail.com>
2022-08-11 18:10:32 -0300

    Fixing image bug

--- src/components/Place/index.jsx
+++ src/components/Place/index.jsx
@@ -1,4 +1,4 @@
 import "./style.css"
 export function Place(){
-    return <div className="place-image"><img src="./src/assets/images/Paris.png" alt="Imagem do lugar"/></div>
+    return <div className="place-image"><img src="./Paris.png" alt="Imagem do lugar"/></div>
 }
