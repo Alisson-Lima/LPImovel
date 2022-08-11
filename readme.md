072306192f92292b33f7ac84c20ee1950b271e3f
Alisson Renan <renalima.rl46@gmail.com>
2022-08-11 17:59:50 -0300

    Adding read.me and image

---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a project to pratice css design and DOM manipulation
--- src/components/Place/index.jsx
+++ src/components/Place/index.jsx
@@ -1,4 +1,4 @@
 import "./style.css"
 export function Place(){
-    return <div className="place-image"><img src="./src/assets/images/fotoParis.jpg" alt="Imagem do lugar"/></div>
+    return <div className="place-image"><img src="./src/assets/images/fotoParis.png" alt="Imagem do lugar"/></div>
 }
