1b9957d5ad46ddbfad2b704a71f6646b894e3181
Alisson Renan <renalima.rl46@gmail.com>
2022-08-13 13:24:44 -0300

    Correcting svgs bug

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -6,13 +6,13 @@ export function Aside(){
             <span className="small-info">
                 <p>
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 12L10 10V5M19 10C19 14.9706 14.9706 19 10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C14.9706 1 19 5.02944 19 10Z" stroke="#252525" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12 12L10 10V5M19 10C19 14.9706 14.9706 19 10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C14.9706 1 19 5.02944 19 10Z" stroke="#252525" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                     : 14 Horas
                 </p>
                 <p>
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M19 10C19 14.9706 14.9706 19 10 19M19 10C19 5.02944 14.9706 1 10 1M19 10C19 10 13.5147 10 10 10C6.48528 10 1 10 1 10M10 19C5.02944 19 1 14.9706 1 10M10 19C7.14508 16.4226 6 13.8462 6 10C6 6.15378 7.14508 3.57736 10 1M10 19C12.8549 16.4226 14 13.8462 14 10C14 6.15378 12.8549 3.57736 10 1M1 10C1 5.02944 5.02944 1 10 1" stroke="#252525" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M19 10C19 14.9706 14.9706 19 10 19M19 10C19 5.02944 14.9706 1 10 1M19 10C19 10 13.5147 10 10 10C6.48528 10 1 10 1 10M10 19C5.02944 19 1 14.9706 1 10M10 19C7.14508 16.4226 6 13.8462 6 10C6 6.15378 7.14508 3.57736 10 1M10 19C12.8549 16.4226 14 13.8462 14 10C14 6.15378 12.8549 3.57736 10 1M1 10C1 5.02944 5.02944 1 10 1" stroke="#252525" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                     : França
                 </p></span>
@@ -20,7 +20,7 @@ export function Aside(){
             <a href="#" className="button-link" onClick={(e) => e.preventDefault()}>
                 Discover
                 <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1.00012 0.999771L8.00012 7.99977L1.00012 14.9998" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M1.00012 0.999771L8.00012 7.99977L1.00012 14.9998" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
             </a>
         </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ export function Header(){
             <div className="search-container">
                 <input type="text" className="search" placeholder='Search'/>
                 <i className="icon"><svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8.76471 8.76471L12 12M10.0588 5.52941C10.0588 8.03094 8.03094 10.0588 5.52941 10.0588C3.02789 10.0588 1 8.03094 1 5.52941C1 3.02789 3.02789 1 5.52941 1C8.03094 1 10.0588 3.02789 10.0588 5.52941Z" stroke="#757575" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M8.76471 8.76471L12 12M10.0588 5.52941C10.0588 8.03094 8.03094 10.0588 5.52941 10.0588C3.02789 10.0588 1 8.03094 1 5.52941C1 3.02789 3.02789 1 5.52941 1C8.03094 1 10.0588 3.02789 10.0588 5.52941Z" stroke="#757575" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                 </i>
             </div>
